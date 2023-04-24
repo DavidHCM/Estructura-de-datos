3a3ca17c99b481bbe9f10b008e67e8297b184020
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-04-24 10:04:55 -0600

    first iter

--- Act-repo-23/Act-1/main.c
+++ Act-repo-23/Act-1/main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#define MAX_HEAP 100
+#define padre(n) (n-1/2)
 
-int main() {
-    printf("Hello, World!\n");
+int heap[MAX_HEAP];
+int size = 0;
+
+void heap_offer(int num);
+void swap(int *a,int *b);
+
+int main()
+{
+    int arr[] = {22,13,15,17,8,26,29,5,14,19,2,7,20,14,29};
+    int i;
+
+    for(i=0;i<15;i++)
+    {
+        heap_offer(arr[i]);
+    }
+
+    for(i=0;i<15;i++)
+    {
+        printf("%d\t",heap[i]);
+    }
+
     return 0;
 }
+
+void heap_offer(int num)
+{
+    int elem = size;
+    // size inicia en 0
+    heap[elem] = num;
+    size++;
+
+    while(elem > 0 && heap[elem] < heap[padre(elem)])
+    {
+        swap(&heap[elem],&heap[padre(elem)]);
+    }
+}
+
+void swap(int *a,int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
