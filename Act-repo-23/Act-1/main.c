9f305eebf79e832defcdea11491bf11f49dd7a24
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-04-24 10:11:31 -0600

    Rev iter

--- Act-repo-23/Act-1/main.c
+++ Act-repo-23/Act-1/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define MAX_HEAP 100
-#define padre(n) (n-1/2)
+#define padre(n) ((n-1)/2)
 
 int heap[MAX_HEAP];
 int size = 0;
@@ -10,7 +10,7 @@ void swap(int *a,int *b);
 
 int main()
 {
-    int arr[] = {22,13,15,17,8,26,29,5,14,19,2,7,20,14,29};
+    int arr[] = {22,13,15,17,8,26,29,5,14,19,2,7,20,13,29};
     int i;
 
     for(i=0;i<15;i++)
@@ -33,9 +33,10 @@ void heap_offer(int num)
     heap[elem] = num;
     size++;
 
-    while(elem > 0 && heap[elem] < heap[padre(elem)])
+    while(elem>0 && heap[elem]<heap[padre(elem)])
     {
         swap(&heap[elem],&heap[padre(elem)]);
+        elem = padre(elem);
     }
 }
 
