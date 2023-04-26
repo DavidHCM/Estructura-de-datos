eafc5e474dbb7b5fe2a50e0fbd416d0f0ad64885
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2023-04-26 10:25:32 -0600

    fixed final iter

--- Act-repo-24/Act-1/priorityqueue.c
+++ Act-repo-24/Act-1/priorityqueue.c
@@ -1,40 +1,10 @@
+/*
+ * priorityqueue.c
+ *
+ *  Created on: 26 abr 2023
+ *      Author: jluis
+ */
 #include "priorityqueue.h"
-
-PRIORITYQUEUE priorityqueue_create(int max_size,COMPAREFUNC cf,PRINTFUNC pf)
-{
-    PRIORITYQUEUE newpq = malloc(sizeof (struct STRPRIORITYQUEUE));
-
-    // Inicializar todos los elementos del arreglo en null
-    for(int j=0;j<max_size;j++)
-    {
-        newpq->arr[j] = NULL;
-    }
-
-    newpq->max_size = max_size;
-    newpq->arr = malloc(max_size * sizeof (TYPE));
-    newpq->size = 0;
-    newpq->cf = cf;
-    newpq->pf = pf;
-    return newpq;
-}
-
-void priorityqueue_offer(PRIORITYQUEUE pq, TYPE data)
-{
-    int idx = pq->size;
-
-
-    pq->arr[idx] = data;
-    pq->size = pq->size + 1;
-
-    //Comparacion entre el elemento y el padre, y de ser  negativo  retorna 0, de lo contrario positivo y los cambia al ser menor
-    while(idx > 0 && pq->cf(pq->arr[idx],pq->arr[parent(idx)]) < 0)
-    {
-        swap(&(pq->arr[idx]),&(pq->arr[parent(idx)]));
-        idx = parent(idx);
-    }
-
-}
-
 
 void swap(TYPE *a,TYPE *b)
 {
@@ -43,7 +13,39 @@ void swap(TYPE *a,TYPE *b)
     *b = temp;
 }
 
-TYPE priority_poll(PRIORITYQUEUE pq)
+PRIORITYQUEUE priorityqueue_create(int max_size,COMPAREFUNC cf,PRINTFUNC pf)
+{
+    PRIORITYQUEUE newpq = malloc(sizeof(struct STRPRIORITYQUEUE));
+    int i;
+    newpq->arr = malloc(max_size * sizeof(TYPE));
+    newpq->max_size = max_size;
+    // Inicializar todos los elementos del arreglo con NULL
+    for(i=0;i<max_size;i++)
+        newpq->arr[i] = NULL;
+
+    newpq->size = 0;
+    newpq->cf = cf;
+    newpq->pf = pf;
+
+    return newpq;
+}
+
+void priorityqueue_offer(PRIORITYQUEUE pq,TYPE data)
+{
+    int idx = pq->size;
+
+
+    pq->arr[idx] = data;
+    pq->size = pq->size + 1;
+
+    while(idx>0 && pq->cf(pq->arr[idx],pq->arr[parent(idx)])<0)
+    {
+        swap(&(pq->arr[idx]),&(pq->arr[parent(idx)]));
+        idx = parent(idx);
+    }
+}
+
+TYPE priorityqueue_poll(PRIORITYQUEUE pq)
 {
     TYPE retval;
     int ilast,ielem,imin;
@@ -53,28 +55,29 @@ TYPE priority_poll(PRIORITYQUEUE pq)
     {
         retval = pq->arr[0];
         pq->size--;
-        ilast = pq->size - 1;
+        ilast = pq->size;
 
         // Poner el último elemento al principio
         pq->arr[0] = pq->arr[ilast];
         ielem=0;
-        while(left(ielem)<pq->size)
+        while(left(ielem)<pq->size) // Mientras tenga hijo izquierdo
         {
             imin = left(ielem);
 
             // Tiene hijo derecho y ademas el hijo derecho
             // es más pequeño que el izquierdo
 
-            if(right(ielem) < pq->size && pq->cf( pq->arr[right(ielem)],pq->arr[left(ielem)])  < 1)
+            if(right(ielem) < pq->size && pq->cf(pq->arr[right(ielem)],pq->arr[left(ielem)]) < 1)
                 imin = right(ielem);
 
-            if(pq->cf(pq->arr[ielem],pq->arr[imin]) > 0)
+
+            if(pq->cf(pq->arr[ielem],pq->arr[imin])>0)
                 swap(&pq->arr[ielem],&pq->arr[imin]);
 
             ielem = imin;
         }
 
     }
-
     return retval;
 }
+
--- Act-repo-24/Act-1/priorityqueue.h
+++ Act-repo-24/Act-1/priorityqueue.h
@@ -1,25 +1,31 @@
+/*
+ * priorityqueue.h
+ *
+ *  Created on: 26 abr 2023
+ *      Author: jluis
+ */
 
-#ifndef ACT_1_PRIORITYQUEUE_H
-#define ACT_1_PRIORITYQUEUE_H
+#ifndef PRIORITYQUEUE_H_
+#define PRIORITYQUEUE_H_
+
 #include "datatypes.h"
+
 #define parent(n) ((n-1)/2)
 #define left(n) (n*2+1)
 #define right(n) (n*2+2)
 
-struct STRPRIORITYQUEUE{
+struct STRPRIORITYQUEUE {
     TYPE *arr;
-    int size;
     int max_size;
+    int size;
     COMPAREFUNC cf;
     PRINTFUNC pf;
 };
 
-typedef struct STRPRIORITYQUEUE *PRIORITYQUEUE;
+typedef struct STRPRIORITYQUEUE * PRIORITYQUEUE;
 
 PRIORITYQUEUE priorityqueue_create(int max_size,COMPAREFUNC cf,PRINTFUNC pf);
-void priorityqueue_offer(PRIORITYQUEUE pq, TYPE data);
-void swap(TYPE *a,TYPE *b);
-TYPE priority_poll(PRIORITYQUEUE pq);
+void priorityqueue_offer(PRIORITYQUEUE pq,TYPE data);
+TYPE priorityqueue_poll(PRIORITYQUEUE pq);
 
-
-#endif //ACT_1_PRIORITYQUEUE_H
+#endif /* PRIORITYQUEUE_H_ */
